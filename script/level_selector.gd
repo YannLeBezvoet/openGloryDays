6111badcc6ba9a0c3c3bc87b1cb1731af16fa109
Yann Le Bezvoet <yann.lebezvoet@gmail.com>
2024-11-05 12:01:41 +0100

    feat: return to main menu

--- script/level_selector.gd
+++ script/level_selector.gd
@@ -3,9 +3,13 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"GridContainer/HBoxContainer/Level 1".grab_focus()
+	$"LevelContainer/HBoxContainer/Level 1".grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/menu.tscn")
