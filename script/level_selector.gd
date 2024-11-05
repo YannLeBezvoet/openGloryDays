3db8eaa79e8a808ec303112644d0a45ff87dcbc1
Yann Le Bezvoet <yann.lebezvoet@gmail.com>
2024-11-05 15:13:41 +0100

    on button pressed for level 1

--- script/level_selector.gd
+++ script/level_selector.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/menu.tscn")
+
+
+func _on_level_1_pressed() -> void:
+	pass # Replace with function body.
