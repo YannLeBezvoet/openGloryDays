d6fa31f457e521bbd6c814ad0b08565c868f4b8c
Yann Le Bezvoet <yann.lebezvoet@gmail.com>
2024-11-09 16:34:11 +0100

    feat: automaticaly move player to the left or right

--- scene/player.gd
+++ scene/player.gd
@@ -1,9 +1,10 @@
 extends Area2D
 @export var speed = 400
-
+enum enum_direction {left, right}
+var direction
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	direction = enum_direction.left
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,6 +14,14 @@ func _process(delta: float) -> void:
 		velocity.y -= 1
 	if Input.is_action_pressed("down"):
 		velocity.y += 1
+	if direction == enum_direction.left:
+		velocity.x -= 1
+	if direction == enum_direction.right:
+		velocity.x += 1
+	if Input.is_action_pressed("right"):
+		direction = enum_direction.right
+	if Input.is_action_pressed("left"):
+		direction = enum_direction.left
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
